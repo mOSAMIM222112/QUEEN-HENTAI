8026aeecd2d59a6faa80007c729e480a0bd7cb98
mOSAMIM222112 <153191809+mOSAMIM222112@users.noreply.github.com>
2023-12-07 18:03:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Queen Hentai', {
+say('MOSAMIM M.S.M', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By Dinuwa Official²⁰²³._`, {
+say(`'${name}' By @aymaneedits²⁰²³._`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
